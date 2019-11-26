0c25f6d9565018a88dbcd4a4a4026f82c0e6a358
Joshua Anderson Slate <josh.a.slate@gmail.com>
2019-11-26 21:55:27 +0000

    Removing useless argument

--- general/context.js
+++ general/context.js
@@ -1,6 +1,6 @@
 /**
  * Write a function that calls getGreeting and returns "Greetings from DevDecoded!"
- * 
+ *
  * HINT: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Function/bind
  *       https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
  *       https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
@@ -8,8 +8,6 @@
 
 function getGreeting() {
   return `Greetings from ${this.name}!`;
-;}
+}
 
-export const returnGreeting = (givenArray = []) => {
-
-};
+export const returnGreeting = () => {};
